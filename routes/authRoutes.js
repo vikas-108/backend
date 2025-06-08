8fadd4af813f501753e386fcc03f9725b0cdea3a
vikas-108 <creative.boy.vk@gmail.com>
2025-06-08 14:45:33 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,13 @@ const express = require('express');
 const router = express.Router();
 const { loginUser, findUserByPhone } = require('../controllers/authController');
 const { addContact } = require('../controllers/authController');
+const {generateOTP, verifyOTP }  = require('../controllers/authController');
 
 router.post('/login', loginUser);
 router.get('/find/:phone', findUserByPhone);
 router.post('/add-contact', addContact);
+router.post('/send-otp', generateOTP);
+router.post('/verify-otp', verifyOTP);
 
 
 module.exports = router;
